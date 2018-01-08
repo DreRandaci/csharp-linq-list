cbf5fcbf2584b820d7da00555b02d32e60a98cc2
Dre mf Randaci <drerandaci@gmail.com>
2018-01-08 14:48:34 -0600

    finished number multiples

--- Program.cs
+++ Program.cs
@@ -12,15 +12,26 @@ namespace linq
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
             IEnumerable<string> LFruits = from fruit in fruits
-                                   where fruit.StartsWith('L')                                   
-                                   orderby fruit ascending
-                                   select fruit;
-            // foreach (var lemonFruits in LFruits)        
-            // {
+                                          where fruit.StartsWith('L')
+                                          orderby fruit ascending
+                                          select fruit;
 
-            // }
             LFruits.ToList().ForEach(f => Console.WriteLine(f));
-                                   
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            List<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0).ToList();
+            
+            foreach (int num in fourSixMultiples)
+            {
+
+            Console.WriteLine(num);
+            }
+
         }
     }
 }
